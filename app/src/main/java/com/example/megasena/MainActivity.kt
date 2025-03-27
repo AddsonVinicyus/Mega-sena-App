d7bfb329ebf1d8baf02f5cf889d24693c605b3b3
Addson <addsonvinicyus@gmail.com>
2025-03-27 12:48:17 -0300

    Finalizado a lógica do app e modificado o visual

--- app/src/main/java/com/example/megasena/MainActivity.kt
+++ app/src/main/java/com/example/megasena/MainActivity.kt
@@ -1,20 +1,77 @@
 package com.example.megasena
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.megasena.databinding.ActivityMainBinding
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var prefs: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        prefs = getSharedPreferences("db", Context.MODE_PRIVATE)
+        val result = prefs.getString("result", null)
+
+
+        with(binding){
+            result?.let{
+                txtResult.text = "Última aposta: $it"
+            }
+
+            btnGen.setOnClickListener {
+
+                val text = editNumber.text.toString()
+
+                numberGenerator(text, txtResult)
+            }
         }
+
+    }
+
+    private fun numberGenerator(text: String, txtResult: TextView){
+        if(text.isEmpty()){
+            displayError()
+            return
+        }
+
+        val qtd = text.toInt()
+
+        if(qtd < 6 || qtd > 15){
+            displayError()
+            return
+        }
+
+        val numbers = mutableSetOf<Int>()
+        val random  = java.util.Random()
+
+        while(numbers.size < qtd){
+            val number = random.nextInt(60)
+            numbers.add(number + 1)
+        }
+
+        txtResult.text = numbers.joinToString(" - ")
+
+        val editor = prefs.edit()
+        editor.putString("result", txtResult.text.toString())
+        editor.apply()
+    }
+
+    private fun displayError(){
+        Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
     }
 }
+
